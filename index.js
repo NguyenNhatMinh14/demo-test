b97018c9a9c483e8d20f52bd433923dc0e07a066
NguyenNhatMinh <nnhatminh14698@gmail.com>
2023-12-18 00:17:37 +0700

    update new

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-var arrSoNhap= [];
-var tongDiem = 0;
-function getNumberFromUser(){
-    var soNhap = document.getElementById('soNhap').value*1
-    arrSoNhap.push(soNhap)
-    document.getElementById('kq1').innerHTML=arrSoNhap
-    for(var i=0; i < arrSoNhap.length;i++){
-        tongDiem += arrSoNhap[i]
-    }
-    return tongDiem
 
+var arrSoNhap = [];
+var tongDiem = 0;
+function getNumberFromUser() {
+    var soNhap = document.getElementById('soNhap').value * 1;
+    arrSoNhap.push(soNhap);
+    document.getElementById('kq1').innerHTML = arrSoNhap;
 }
-console.log(arrSoNhap)
-console.log(tongDiem)
-document.getElementById('btn1').onclick = getNumberFromUser
-//câu 1 Tổng số dương
+document.getElementById('btn1').onclick = getNumberFromUser;
+function sum() {
+    for (var i = 0; i < arrSoNhap.length; i++) {
+        tongDiem += arrSoNhap[i];
+    }
+    document.getElementById('kq2').innerHTML = tongDiem;
+    return tongDiem
+}
+document.getElementById('btn2').onclick = sum;
+
