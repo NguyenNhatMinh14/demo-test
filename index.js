d43f64d41e734ecb6825e7943c395a63bef95bc7
NguyenNhatMinh <nnhatminh14698@gmail.com>
2023-12-19 00:30:17 +0700

    update 3

--- index.js
+++ index.js
@@ -59,16 +59,21 @@ function soChanCuoiCung(){
 }
 document.getElementById('btn6').onclick=soChanCuoiCung
 //-------Câu 6
-const arr = [3, 2, 3, 4, 5, 6];
-// arr[0] arr[2]
-var soA = document.getElementById().value * 1;
-var soB = document.getElementById().value * 1;
-var trungGian = arr[soA];
-arr[soA] = arr[soB]; // arr[2] = arr[5]
-arr[soB] = trungGian;
 function convert(){
     var soA =document.getElementById('soA').value*1
     var soB = document.getElementById('soB').value*1
-    var trungGian = soB
-    arrSoNhap[]
+    var trungGian = arrSoNhap[soA]
+    arrSoNhap[soA]=arrSoNhap[soB]
+    arrSoNhap[soB]=trungGian
+    document.getElementById('kq7').innerHTML=arrSoNhap
 }
+document.getElementById('btn7').onclick=convert
+
+//---- Câu 7
+function tangDan(){
+    var newArrSoNhap = arrSoNhap.sort(function(a,b){
+        return a-b
+    })
+    document.getElementById('kq8').innerHTML=newArrSoNhap
+}
+document.getElementById('btn8').onclick=tangDan
